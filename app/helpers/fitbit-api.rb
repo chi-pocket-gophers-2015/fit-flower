4a82fe8e8b90f54e47d8d9301845dee27d8c3ccb
lcmbl <cbarentine@gmail.com>
2015-05-15 10:35:23 -0500

    Added client and migration / model

--- app/helpers/fitbit-api.rb
+++ app/helpers/fitbit-api.rb
@@ -12,16 +12,20 @@ helpers do
     end
 
     client = Fitgem::Client.new(config[:oauth])
+    # store client in session for later access
+    session[:client] = client
 
     # With the token and secret, we will try to use them
     # to reconstitute a usable Fitgem::Client
-    # if config[:oauth][:token] && config[:oauth][:secret]
-      # begin
-      #   access_token = client.reconnect(config[:oauth][:token], config[:oauth][:secret])
-      # rescue Exception => e
-      #   puts "Error: Could not reconnect Fitgem::Client due to invalid keys in .fitgem.yml"
-      #   exit
-      # end
+    if session[:access_token]
+      begin
+        session[:access_token] = client.reconnect(session[:access_token].token, session[:access_token].secret)
+        return
+      rescue Exception => e
+        puts "Error: Could not reconnect Fitgem::Client due to invalid keys in .fitgem.yml"
+        exit
+      end
+    end
     # Without the secret and token, initialize the Fitgem::Client
     # and send the user to login and get a verifier token
     # else
@@ -34,37 +38,35 @@ helpers do
       return "https://www.fitbit.com/oauth/authorize?oauth_token=#{session[:request_token].token}"
     end
 
-    def new_client
+    def authorize_user(verifier)
       begin
-        access_token = client.authorize(token, secret, { :oauth_verifier => verifier })
+        session[:access_token] = session[:client].authorize(session[:request_token].token, session[:request_token].secret, { :oauth_verifier => verifier })
       rescue Exception => e
         puts "Error: Could not authorize Fitgem::Client with supplied oauth verifier"
         exit
       end
 
-      puts 'Verifier is: '+verifier
-      puts "Token is:    "+access_token.token
-      puts "Secret is:   "+access_token.secret
+      # puts 'Verifier is: '+verifier
+      # puts "Token is:    "+access_token.token
+      # puts "Secret is:   "+access_token.secret
 
-      user_id = client.user_info['user']['encodedId']
-      puts "Current User is: "+user_id
+      # user_id = session[:client].user_info['user']['encodedId']
+      # puts "Current User is: "+user_id
 
-      config[:oauth].merge!(:token => access_token.token, :secret => access_token.secret, :user_id => user_id)
+      # config[:oauth].merge!(:token => access_token.token, :secret => access_token.secret, :user_id => user_id)
+      # HERE IS WHERE WE WOULD WRITE TOKENS TO DB OR SESSION
 
       # Write the whole oauth token set back to the config file
       # File.open(".fitgem.yml", "w") {|f| f.write(config.to_yaml) }
 
     # end
-    return client
+    # return true
   end
 
   def client
-    # @client ||= Fitgem::Client.new(@config[:oauth])
+    session[:client]
   end
 
 
-  def method_name
-
-  end
 
 end
